399154ef1071e6ce86ba163c9be581727d58f717
Orion (Theunkn0wn1) <thHunkn0WNd@gmail.com>
2019-05-09 00:48:54 -0700

    began building the word tables for EEPROm write

--- firmware_builder.py
+++ firmware_builder.py
@@ -1,10 +1,17 @@
 import argparse
+import typing
 
 from sap1 import instruction_set
+from sap1.compiler.word import Word, address
 from sap1.instruction_set.microcode import Microcode
 
 
-def table_dump():
+def table_human_dump():
+    """
+    Dump all the words into a human-readable table
+    Returns:
+
+    """
     print(f"{'Truth Table':-^120}")
     header = f"{'instruction': >12}| {'opcode': ^6}| {'t': ^3}|"
     for key in vars(Microcode()):
@@ -20,6 +27,32 @@ def table_dump():
                 line += f"{bit: ^5}|"
 
             print(line)
+
+
+def table_human_hex():
+    """
+    dump instructions into a human_readable
+    Returns:
+
+    """
+    print(f"{'Word table':-^120}")
+    header = f"{'mnemonic': >12}| {'t': ^3}| {'address': ^9}| {'word': ^20} |"
+
+    print (header)
+    for mnemonic, instruction in instructions.items():
+        print(f"{'.':.^120}")
+        for i, code in enumerate(instruction.states):
+            addr = address(instruction, i)
+            word = Word(address=addr, code=instruction.states[i])
+            line = f"{mnemonic: >12}| {i: ^3}| {addr.to01(): ^9}| {word.word.to01(): ^20} |"
+
+            print(line)
+
+
+
+
+def table_hex() -> typing.Dict:
+    ...
 
 
 if __name__ == '__main__':
@@ -44,6 +77,7 @@ ______ _   _ _____ _    ______ ___________
     parser = argparse.ArgumentParser("SAP-1 Firmware Builder")
     parser.add_argument("--truth-table", action="store_true",
                         help="display a human-readable truth table of firmware package")
+    parser.add_argument("--word-table", action="store_true")
     args = parser.parse_args()
     instructions = {'LDA': instruction_set.lda(ptr=0),
                     'ADD': instruction_set.add(ptr=0),
@@ -52,6 +86,9 @@ ______ _   _ _____ _    ______ ___________
                     'JMP': instruction_set.jmp(ptr=0),
                     'HLT': instruction_set.hlt(), }
     if args.truth_table:
-        table_dump()
+        table_human_dump()
+
+    if args.word_table:
+        table_human_hex()
 
     print(f"{'  Done.  ':=^120}")
--- sap1/compiler/word.py
+++ sap1/compiler/word.py
@@ -1,9 +1,12 @@
+from dataclasses import dataclass
+
 from bitarray import bitarray
 
 from sap1.instruction_set import Instruction
+from sap1.instruction_set.microcode import Microcode
 
 
-def address(instruction: Instruction, time: int) -> int:
+def address(instruction: Instruction, time: int) -> bitarray:
     """
     Calculate the address of an instruction
 
@@ -17,4 +20,20 @@ def address(instruction: Instruction, time: int) -> int:
     time_bits = bitarray(f"{time:0>3b}")
     word: bitarray = instruction.opcode + time_bits
     # convert to bin, then to int
-    return int(word.to01(), 2)
+    return word
+
+
+@dataclass
+class Word:
+    address: bitarray
+    code: Microcode
+
+    @property
+    def word(self):
+        return self.code.dump()
+
+    def __index__(self) -> int:
+        ...
+
+    def __int__(self) -> int:
+        ...
