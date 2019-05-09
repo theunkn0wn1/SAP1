355bbcc912dd97c5d655ce26a08f0e1f3302c020
Orion (Theunkn0wn1) <thHunkn0WNd@gmail.com>
2019-05-08 21:55:10 -0700

    Better formatting on `poc.asm`

--- poc.asm
+++ poc.asm
@@ -3,7 +3,8 @@
 # while True:
 #   a += b
 
- LDA 15
-    ADD 14   # i like pizza
-    JMP 1  # while True
+LDA 15  # load 0xF into A
+    ADD 14   # add 0xD to A
+    SUB 13  # subtract 0xC from A
+    JMP 1  # while True GOTO add
  HLT # totally unreachable but whatever, probably a good thing to have
