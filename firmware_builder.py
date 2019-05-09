446168eda5963bfada0e0b20ea028158588bc39a
Orion (Theunkn0wn1) <thHunkn0WNd@gmail.com>
2019-05-08 23:47:53 -0700

    Add opcode to truth table

--- firmware_builder.py
+++ firmware_builder.py
@@ -1,18 +1,25 @@
+import argparse
+
 from sap1 import instruction_set
 from sap1.instruction_set.microcode import Microcode
 
 
-def fmt_table():
-    header = f"{'instruction': >12}| {'step': ^5}|"
+def table_dump():
+    print(f"{'Truth Table':-^120}")
+    header = f"{'instruction': >12}| {'t': ^3}|"
     for key in vars(Microcode()):
         header += f"{key: ^5}|"
+
+    header += f"{'opcode': ^6}|"
     print(header)
     for mnemonic, instruction in instructions.items():
         print(f"{'.':.^120}")
         for i, code in enumerate(instruction.states):
-            line = f"{mnemonic: >12}| {i: ^5}|"
+            line = f"{mnemonic: >12}| {i:0>3b}|"  # FIXME check what max T should be in binary
             for bit in vars(code).values():
                 line += f"{bit: ^5}|"
+
+            line += f"{instruction.opcode.to01(): ^6}|"
             print(line)
 
 
@@ -35,13 +42,17 @@ ______ _   _ _____ _    ______ ___________
 
     """)
     print(f"{'  SAP-1 Firmware Builder by Joshua (Theunkn0wn1) Salzedo  ':-^120}")
+    parser = argparse.ArgumentParser("SAP-1 Firmware Builder")
+    parser.add_argument("--truth-table", action="store_true",
+                        help="display a human-readable truth table of firmware package")
+    args = parser.parse_args()
     instructions = {'LDA': instruction_set.lda(ptr=0),
                     'ADD': instruction_set.add(ptr=0),
                     'SUB': instruction_set.sub(ptr=0),
                     'OUT': instruction_set.out(ptr=0),
                     'JMP': instruction_set.jmp(ptr=0),
                     'HLT': instruction_set.hlt(), }
-
-    fmt_table()
+    if args.truth_table:
+        table_dump()
 
     print(f"{'  Done.  ':=^120}")
