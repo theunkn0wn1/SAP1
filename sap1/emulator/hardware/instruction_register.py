3f116cf881c2b21aaf26eb716bd19a3e3ff37514
Orion (Theunkn0wn1) <thHunkn0WNd@gmail.com>
2019-05-10 17:42:20 -0700

    Implemented the Instruction register

---
+++ sap1/emulator/hardware/instruction_register.py
@@ -0,0 +1,35 @@
+from bitarray import bitarray
+
+from .component_bases import BusComponent
+from .register import Register
+
+
+class InstructionRegister(Register):
+    """
+    Instruction register subclass
+    """
+
+    @property
+    def opcode(self) -> bitarray:
+        """
+        Stored opcode
+
+        Returns:
+            size 4 bitarray opcode
+        """
+        return self.memory[:4]
+
+    @property
+    def operand(self):
+        """
+        The four least significant bits, representing the instruction's operand
+
+        Returns:
+            size 4 bitarray operand
+        """
+        return self.memory[4:]
+
+    def write(self) -> None:
+        # the IR only outputs operand, puling the 4 MSB bits low
+        word = bitarray('0000') + self.operand
+        BusComponent.bus_state = word
