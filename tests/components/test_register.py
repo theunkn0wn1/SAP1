213720d88553a54c54b4809f153cd10ca4cf6181
Orion (Theunkn0wn1) <thHunkn0WNd@gmail.com>
2019-05-11 14:22:31 -0700

    added check against a different register for correct  control parsing

--- tests/components/test_register.py
+++ tests/components/test_register.py
@@ -7,9 +7,13 @@ from sap1.types import HIGH
 
 
 @pytest.mark.parametrize("value", [1, 2, 255, 12])
-def test_read(a_register_fx, value):
+def test_read(a_register_fx, b_register_fx, value):
+    """
+    Tests the Register's ability to selectively read from the bus
+    """
     BusComponent.bus_state = bitarray(f"{value:0>b}")
     with a_register_fx.signal(Microcode(AI=HIGH)):
         a_register_fx._clock_tick()
 
-    assert int(a_register_fx) == value, "register did not contain expected value!"
+    assert int(a_register_fx) == value, " A register did not contain expected value!"
+    assert int(b_register_fx) == 0, "B register incorrectly read from the bus"
