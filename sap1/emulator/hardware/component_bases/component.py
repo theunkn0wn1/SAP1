78e91f9cfcc022151b687f8445625f94bcb60bba
Orion (Theunkn0wn1) <thHunkn0WNd@gmail.com>
2019-05-11 12:47:43 -0700

    implement signal context manager

--- sap1/emulator/hardware/component_bases/component.py
+++ sap1/emulator/hardware/component_bases/component.py
@@ -1,9 +1,28 @@
+import contextlib
 import dataclasses
+import functools
 import typing
 
-from sap1.instruction_set.microcode import Microcode
+from sap1.instruction_set.microcode import Microcode, NO_OP
 
 
 @dataclasses.dataclass
 class Component:
     control_word: typing.ClassVar[Microcode] = Microcode()
+
+    @classmethod
+    @functools.wraps(Microcode)
+    @contextlib.contextmanager
+    def signal(cls, word: Microcode) -> None:
+        """
+        temporally  set the control word to `word`, reset to :obj:`NO_OP` on exit
+
+        Args:
+            word(Microcode): control word to use on entry
+
+        """
+        cls.control_word = word
+
+        yield
+
+        cls.control_word = NO_OP
