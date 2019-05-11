9a520f4dcd19290be143c64719730509fbad84fe
Orion (Theunkn0wn1) <thHunkn0WNd@gmail.com>
2019-05-11 15:50:43 -0700

    cleanup after ourselves

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 from sap1.emulator.hardware.alu import ALU
+from sap1.emulator.hardware.component_bases import BusComponent
 from sap1.emulator.hardware.instruction_register import InstructionRegister
 from sap1.emulator.hardware.register import Register
 
@@ -23,3 +24,9 @@ def instruction_register_fx() -> InstructionRegister:
 @pytest.fixture
 def alu_fx(a_register_fx, b_register_fx) -> ALU:
     return ALU(A_REGISTER=a_register_fx, B_REGISTER=b_register_fx)
+
+
+@pytest.fixture(autouse=True)
+def reset_globals_fx():
+    yield
+    BusComponent.bus_state.setall(0)
