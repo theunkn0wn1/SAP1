7160e647c803038830d3aadf336cdff5b6807059
Orion (Theunkn0wn1) <thHunkn0WNd@gmail.com>
2019-05-12 11:55:24 -0700

    It would help to actually initialize the program counter

--- sap1/emulator/__main__.py
+++ sap1/emulator/__main__.py
@@ -7,7 +7,7 @@ from humanfriendly import AutomaticSpinner
 from humanfriendly.cli import warning
 
 from sap1.emulator.hardware.register import RegisterReadOnly
-from sap1.types import Bit, LOW
+from sap1.types import Bit, LOW, HIGH
 from . import hardware
 
 LOG = logging.getLogger(f"sap1.{__name__}")
@@ -48,16 +48,17 @@ if __name__ == '__main__':
 
     memory_target = pathlib.Path(namespace.memory)
     if not memory_target.exists():
-        warning(f"unable to locate memory profile {memory_target}. Stop.")
+        warning(f"unable to locate memory profile {memory_target} Stop.")
         exit(1)
 
     if not memory_target.is_file():
-        warning(f"{memory_target} is not a file. Stop.")
+        warning(f"{memory_target.resolve()} is not a file. Stop.")
         exit(1)
 
     with AutomaticSpinner("pre flight checks passed. initializing...."):
         print("initializing components...")
 
+        program_counter = hardware.ProgramCounter()
         instruction_register = hardware.InstructionRegister()
         mar = hardware.Mar()
         control = hardware.ControlUnit(instruction_register)
@@ -80,15 +81,18 @@ if __name__ == '__main__':
 
         with AutomaticSpinner("executing program..."):
             # run til we reach a HALT or a OUT instruction
-            while (control.word.HLT == Bit(LOW)) and (control.word.OI == Bit(LOW)):
+            while (control.word.HLT == Bit(HIGH)) or (control.word.OI == Bit(HIGH)):
                 control._clock_tick()
+
+            # step one forward so the output operation can run its course
+            control._clock_tick()
             print(f"result:= {output_register.memory}")
             print(f"control word:= {control.word}")
     else:
         print("running in interactive mode... press <enter> to step forward")
         while (control.word.HLT == Bit(LOW)) and (control.word.OI == Bit(LOW)):
             print(f"{'stepping...':-^120}")
-            print(f"timestep: {control.time_step} \t program_counter {progra}")
+            print(f"timestep: {control.time_step} \t program_counter {program_counter}")
             print(f"opcode: {instruction_register.opcode}\toperand{instruction_register.operand}")
             print(f"control word: {control.word} \t instruction_register {instruction_register}")
             print(f"mar: {mar}\tram value:{ram.value}")
