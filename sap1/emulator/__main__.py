26d751d8147638e52f0ec39b319870a5f4de856e
Orion (Theunkn0wn1) <thHunkn0WNd@gmail.com>
2019-05-12 16:31:07 -0700

    extracted runtime and component init

--- sap1/emulator/__main__.py
+++ sap1/emulator/__main__.py
@@ -2,12 +2,9 @@ import argparse
 import logging
 import pathlib
 
-from humanfriendly import AutomaticSpinner
 from humanfriendly.cli import warning
 
-from sap1.emulator._runtime import load_memory_from_buffer
-from sap1.emulator.hardware.register import RegisterReadOnly
-from . import hardware
+from sap1.emulator._runtime import runtime
 
 LOG = logging.getLogger(f"sap1.{__name__}")
 
@@ -44,54 +41,4 @@ if __name__ == '__main__':
         warning(f"{memory_target.resolve()} is not a file. Stop.")
         exit(1)
 
-    with AutomaticSpinner("pre flight checks passed. initializing...."):
-        print("initializing components...")
-
-        program_counter = hardware.ProgramCounter()
-        instruction_register = hardware.InstructionRegister()
-        mar = hardware.Mar()
-        control = hardware.ControlUnit(instruction_register)
-        ram = hardware.Ram(mar)
-        output_register = RegisterReadOnly(name='O')
-        register_a = hardware.Register('A')
-        register_b = hardware.Register('B')
-        alu = hardware.ALU(register_a, register_b)
-
-        print(f"OK.\nLoading memory profile {memory_target.resolve()} ....")
-        raw_memory = memory_target.read_text()
-        print("OK.")
-
-        print("Applying memory profile....")
-        load_memory_from_buffer(raw_memory, mar, ram)
-
-        print("OK.")
-    print(f"{' Ready. ':-^120}")
-    if namespace.non_interactive:
-        print("running in non-interactive mode...")
-
-        with AutomaticSpinner("executing program..."):
-            # run til we reach a HALT or a OUT instruction
-            should_stop = False
-            while not should_stop:
-                should_stop = control.word.HLT or (control.word.OI and namespace.pause_on_out)
-                control._clock_tick()
-
-            # step one forward so the output operation can run its course
-            control._clock_tick()
-            print(f"result:= {output_register.memory}")
-            print(f"control word:= {control.word}")
-    else:
-        print("running in interactive mode... press <enter> to step forward")
-        should_stop = False
-        while not should_stop:
-            control._clock_tick()
-            print(f"{'stepping...':-^120}")
-            print(f"timestep: {control.time_step} \t program_counter {program_counter}\n")
-            print(f"opcode: {instruction_register.opcode}\toperand{instruction_register.operand}\n")
-            print(f"control word: {control.word} \n instruction_register {instruction_register}\n")
-            print(f"mar: {mar}\tram value:{ram.value}\n")
-            print(f"a_register: {register_a.memory}\t b_register: {register_b.memory}\n")
-            print(f"output register: {output_register.memory}\n")
-            input("press enter to step forward")
-
-            should_stop = control.word.HLT or control.word.OI
+    runtime(memory_target, namespace)
--- sap1/emulator/_runtime.py
+++ sap1/emulator/_runtime.py
@@ -1,6 +1,12 @@
-from bitarray import bitarray
+import pathlib
+from argparse import Namespace
 
+from bitarray import bitarray
+from humanfriendly import AutomaticSpinner
+
+from sap1.emulator import hardware
 from sap1.emulator.hardware import Mar, Ram
+from sap1.emulator.hardware.register import RegisterReadOnly
 
 
 def load_memory_from_buffer(buffer: str, mar: Mar, ram: Ram):
@@ -13,3 +19,64 @@ def load_memory_from_buffer(buffer: str, mar: Mar, ram: Ram):
         ram.value = bitarray(line)
         assert len(ram.value) == 8, "invalid memory word!"
         print(ram.value)
+
+
+def runtime(memory_path: pathlib.Path, configuration: Namespace):
+    with AutomaticSpinner("pre flight checks passed. initializing...."):
+        print("initializing components...")
+
+        (control, instruction_register, mar, output_register, program_counter, ram,
+         register_a, register_b, alu) = init_components()
+
+        print(f"OK.\nLoading memory profile {memory_path.resolve()} ....")
+        raw_memory = memory_path.read_text()
+        print("OK.")
+
+        print("Applying memory profile....")
+        load_memory_from_buffer(raw_memory, mar, ram)
+
+        print("OK.")
+    print(f"{' Ready. ':-^120}")
+    if configuration.non_interactive:
+        print("running in non-interactive mode...")
+
+        with AutomaticSpinner("executing program..."):
+            # run til we reach a HALT or a OUT instruction
+            should_stop = False
+            while not should_stop:
+                should_stop = control.word.HLT or (control.word.OI and configuration.pause_on_out)
+                control._clock_tick()
+
+            # step one forward so the output operation can run its course
+            control._clock_tick()
+            print(f"result:= {output_register.memory}")
+            print(f"control word:= {control.word}")
+    else:
+        print("running in interactive mode... press <enter> to step forward")
+        should_stop = False
+        while not should_stop:
+            control._clock_tick()
+            print(f"{'stepping...':-^120}")
+            print(f"timestep: {control.time_step} \t program_counter {program_counter}\n")
+            print(f"opcode: {instruction_register.opcode}\toperand{instruction_register.operand}\n")
+            print(f"control word: {control.word} \n instruction_register {instruction_register}\n")
+            print(f"mar: {mar}\tram value:{ram.value}\n")
+            print(f"a_register: {register_a.memory}\t b_register: {register_b.memory}\n")
+            print(f"output register: {output_register.memory}\n")
+            input("press enter to step forward")
+
+            should_stop = control.word.HLT or control.word.OI
+
+
+def init_components():
+    program_counter = hardware.ProgramCounter()
+    instruction_register = hardware.InstructionRegister()
+    mar = hardware.Mar()
+    control = hardware.ControlUnit(instruction_register)
+    ram = hardware.Ram(mar)
+    output_register = RegisterReadOnly(name='O')
+    register_a = hardware.Register('A')
+    register_b = hardware.Register('B')
+    alu = hardware.ALU(register_a, register_b)
+    return (control, instruction_register, mar, output_register, program_counter, ram, register_a,
+            register_b, alu)
