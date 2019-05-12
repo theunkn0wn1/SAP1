82f42d2ff85c6d858c2ad81563fd24e09adb6f97
Orion (Theunkn0wn1) <thHunkn0WNd@gmail.com>
2019-05-12 16:17:01 -0700

    More work on the pretty UI

--- sap1/emulator/_screens.py
+++ sap1/emulator/_screens.py
@@ -1,23 +1,29 @@
-import time
-
 from asciimatics.effects import Print
 from asciimatics.exceptions import NextScene, StopApplication
 from asciimatics.renderers import FigletText
 from asciimatics.scene import Scene
 from asciimatics.screen import ManagedScreen
-from asciimatics.widgets import Frame, Layout, Button
+from asciimatics.widgets import Frame, Layout, Button, Label
 
 
-def demo():
+def run_menu_system():
     banner = FigletText("SAP-1 Emulator", font="doom")
     with ManagedScreen() as screen:
-        effects = [
-            Print(screen, banner, 0, colour=screen.COLOUR_GREEN, attr=screen.A_BOLD),
-            Main(screen, 35, 120,y=10)
+        scenes = [
+            Scene([
+                Print(screen, banner, 0, colour=screen.COLOUR_GREEN, attr=screen.A_BOLD),
+                Main(screen, 35, 110, y=10),
+            ], name="Main"),
+
+            Scene([
+                Print(screen, banner, 0, colour=screen.COLOUR_GREEN, attr=screen.A_BOLD),
+                HardwareView(screen, 35, 100, y=10),
+            ],
+                name="HardwareView")
         ]
-        screen.play([Scene(effects)])
+
+        screen.play(scenes)
         screen.refresh()
-        time.sleep(2)
 
 
 class Main(Frame):
@@ -29,10 +35,15 @@ class Main(Frame):
                          y, has_shadow, reduce_cpu, is_modal, can_scroll)
 
         # Create the form for displaying the list of contacts.
-        layout = Layout([4,2], fill_frame=True)
+        layout = Layout([6], fill_frame=True)
         self.add_layout(layout)
-        layout.add_widget(Button("View Memory", on_click=self.on_view_memory))
-        layout.add_widget(Button("View hardware", on_click=self.on_view_memory))
+        layout.add_widget(Label("SAP-1 Emulator Version 0.0.0 by Joshua (Theunkn0wn1) Salzedo"))
+        layout.add_widget(Button("Load Program...", on_click=self.on_view_memory))
+        layout.add_widget(Button("View hardware...", on_click=self.on_view_hardware))
+        layout.add_widget(Button("Advanced options...", on_click=...))
+        layout.add_widget(Button("Execute program", on_click=...))
+        layout.add_widget(Button("Reset", on_click=...))
+
         layout2 = Layout([1, 1, 1, 1])
         self.add_layout(layout2)
         layout2.add_widget(Button("QUIT", self._ok), 0)
@@ -46,6 +57,9 @@ class Main(Frame):
     def on_view_memory(self):
         raise NextScene("MemoryView")
 
+    def on_view_hardware(self):
+        raise NextScene("HardwareView")
+
     def _ok(self):
         self.save()
         raise StopApplication("goodbye")
@@ -56,7 +70,33 @@ class Main(Frame):
 
 
 class MemoryView(Frame):
+
+    def __init__(self, screen, height, width, data=None, on_load=None, has_border=True,
+                 hover_focus=False, name=None, title=None, x=None, y=None, has_shadow=False,
+                 reduce_cpu=False, is_modal=False, can_scroll=True):
+        super().__init__(screen, height, width, data, on_load, has_border, hover_focus, name, title, x,
+                         y, has_shadow, reduce_cpu, is_modal, can_scroll)
+        self.fix()
+
+
+class HardwareView(Frame):
+    def __init__(self, screen, height, width, data=None, on_load=None, has_border=True,
+                 hover_focus=False, name=None, title=None, x=None, y=None, has_shadow=False,
+                 reduce_cpu=False, is_modal=False, can_scroll=True):
+        super().__init__(screen, height, width, data, on_load, has_border, hover_focus, name, title, x,
+                         y, has_shadow, reduce_cpu, is_modal, can_scroll)
+
+        layout = Layout([2])
+        self.add_layout(layout)
+
+        layout.add_widget(Button("go back", on_click=self.on_click))
+        self.fix()
+
+    def on_click(self):
+        raise NextScene("Main")
+
     ...
 
 
-demo()
+if __name__ == "__main__":
+    run_memu_system()
